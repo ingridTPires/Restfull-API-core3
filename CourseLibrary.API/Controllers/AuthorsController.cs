39b9f23905de822088b54f002f1a020dcba74542
Ingrid Teixeira <ingrid.teixeirap@gmail.com>
2020-02-03 16:41:39 -0300

    Usando statuscode.

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -5,6 +5,7 @@ using System;
 namespace CourseLibrary.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -14,11 +15,23 @@ namespace CourseLibrary.API.Controllers
             _courseLibraryRepository = courseLibraryRepository ??
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
-        [HttpGet("api/authors")]
+        [HttpGet()]
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authorsFromRepo);
+            return Ok(authorsFromRepo);
+        }
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            // já que é utilizada ao final do método, não é certo usar um método Exists do reposíitório 
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorId == null)
+            {
+                return NotFound();
+            }
+            return Ok(authorFromRepo);
         }
     }
 }
